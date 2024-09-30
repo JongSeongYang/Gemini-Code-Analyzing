383786729701568ebeaf4ee7651ece34a24c467b
Jeremy-kis <jeremy@kis-global.com>
2024-09-30 14:51:31 +0900

    fit : Read C File

--- src/CodeAnalyzing.py
+++ src/CodeAnalyzing.py
@@ -2,7 +2,7 @@ import pathlib
 import textwrap
 
 import google.generativeai as genai
-
+import aspose.words as aw
 from IPython.display import display
 from IPython.display import Markdown
 
@@ -23,10 +23,22 @@ genai.configure(api_key=GOOGLE_API_KEY)
 #   if 'generateContent' in m.supported_generation_methods:
 #     print(m.name)
 
+# C 파일 읽어오기
+filename = 'tr_FNCE70OP0008R.h'
+f = open(filename, 'rt', encoding='UTF8')     # mode = 부분은 생략해도 됨
+lines = f.read()
+header_txt = ""
+
+for line in lines:
+    header_txt += line
+# print(header_txt)
+
 model = genai.GenerativeModel('gemini-pro') # 텍스트 전용 모델
 # 텍스트 생성
-response = model.generate_content("파이썬 설명해줘")
+request = "아래 C 코드 Input, Output 정리해서 한국어와 영어 두 개 만들어줘\n"
+response = model.generate_content(request + header_txt)
 
 # 답변 내용 보기
 print(response.text)
 to_markdown(response.text)
+
